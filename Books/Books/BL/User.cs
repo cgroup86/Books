0e6c3d55c6ccbd6856aec4602ff3fae009cab800
mrabofull <mrabofull@gmail.com>
2024-08-01 16:26:30 +0300

    c

---
+++ Books/Books/BL/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Books.BL
+{
+    public class User
+    {
+        int id;
+        string name;
+        string email;
+        string password;
+        bool isAdmin;
+        bool banned;
+    }
+}
