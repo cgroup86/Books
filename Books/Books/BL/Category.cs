7bdfdda2d28618b6861bcd693650c1719d36d69c
mrabofull <mrabofull@gmail.com>
2024-08-12 16:41:04 +0300

    c

---
+++ Books/Books/BL/Category.cs
@@ -0,0 +1,28 @@
+﻿namespace Books.BL
+{
+    public class Category
+    {
+        string name;
+
+        public Category(string name)
+        {
+            this.Name = name;
+        }
+
+        public Category() { }
+
+        public string Name { get => name; set => name = value; }
+
+        //public Category Insert()
+        //{
+        //    DBservices dBservices = new DBservices();
+        //    return dBservices.insertAuthor(this);
+        //}
+
+        public int insertCategory()
+        {
+            DBservices dBservices = new DBservices();
+            return dBservices.insertCategory(this);
+        }
+    }
+}
---
+++ Books/Books/Controllers/AuthorsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Books.BL;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Author> Get(int page = 1, int pageSize = 10)
+        {
+            Author author = new Author();
+            var totalCount = author.Read().Count();
+            var totalPages = (int)Math.Ceiling((decimal)totalCount / pageSize);
+            var productsPerPage = author.Read().Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            return productsPerPage;
+            //return author.Read();
+        }
+
+        ////// GET api/<ValuesController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //POST api/<ValuesController>
+        [HttpPost]
+        public int Post([FromBody] Author author)
+        {
+            return author.Insert();
+        }
+
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("InsertImages/AuthorName/{name}/AuthorImage/{image}/Description/{description}")]
+
+        public int PutImagesOfAuthors(string name, string image, string description)
+        {
+            Author author = new Author();
+            return author.InsertImagesOfAuthor(name, image, description);
+        }
+
+        //// DELETE api/<ValuesController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
